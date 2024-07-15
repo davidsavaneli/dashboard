5789825b84774d3cf964016c7eff572ef16817cc
sava <savaneli.david@gmail.com>
2024-07-15 17:16:25 +0400

    Switch

--- src/pages/Switch/index.tsx
+++ src/pages/Switch/index.tsx
@@ -1,11 +1,40 @@
-import { ContentLayout, Card, Switch } from '../../components'
+import { ContentLayout, Card, Switch, Checkbox } from '../../components'
 
 const SwitchPage = () => {
   return (
-    <ContentLayout label='Switch Page'>
-      <Card title='Switch'>
-        <Switch label='Switch' />
-      </Card>
+    <ContentLayout label='Switch Component'>
+      <div className='row'>
+        <div className='col-6'>
+          <Card title='Default'>
+            <Switch label='Switch' />
+          </Card>
+          <br />
+          <Card title='Colors'>
+            <Switch defaultChecked color='primary' />
+            <Switch defaultChecked color='primaryLight' />
+            <Switch defaultChecked color='secondary' />
+            <Switch defaultChecked color='dark' />
+            <Switch defaultChecked color='medium' />
+            <Switch defaultChecked color='light' />
+            <Switch defaultChecked color='success' />
+            <Switch defaultChecked color='error' />
+            <Switch defaultChecked color='info' />
+            <Switch defaultChecked color='warning' />
+          </Card>
+        </div>
+        <div className='col-6'>
+          <Card title='Sizes'>
+            <Switch size='sm' />
+            <Switch size='md' />
+            <Switch size='lg' />
+            <br />
+            <br />
+            <Switch label='Switch' size='sm' />
+            <Switch label='Switch' size='md' />
+            <Switch label='Switch' size='lg' />
+          </Card>
+        </div>
+      </div>
     </ContentLayout>
   )
 }
