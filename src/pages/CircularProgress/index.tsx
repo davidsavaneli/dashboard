378c1f17b5974eac52287ce87df73f88cb8bc188
sava <savaneli.david@gmail.com>
2024-07-17 15:23:57 +0400

    circular progress page

--- src/pages/CircularProgress/index.tsx
+++ src/pages/CircularProgress/index.tsx
@@ -3,106 +3,56 @@ import { ContentLayout, Card, CircularProgress, Text, Divider } from '../../comp
 const CircularProgressPage = () => {
   return (
     <ContentLayout label='Circular Progress Component'>
-      <div className='row'>
-        <div className='col-6'>
-          <Card title='Default'>
-            <CircularProgress />
-            <br />
-            <br />
-            <Text>Size: md / Color: Primary</Text>
-          </Card>
-          <br />
-          <Card title='Sizes'>
-            <CircularProgress size='xs' />
-            <br />
-            <br />
-            <Text>Size: xs</Text>
-            <Divider />
-            <CircularProgress size='sm' />
-            <br />
-            <br />
-            <Text>Size: sm</Text>
-            <Divider />
-            <CircularProgress size='md' />
-            <br />
-            <br />
-            <Text>Size: md</Text>
-            <Divider />
-            <CircularProgress size='lg' />
-            <br />
-            <br />
-            <Text>Size: lg</Text>
-            <Divider />
-            <CircularProgress size='xl' />
-            <br />
-            <br />
-            <Text>Size: xl</Text>
-            <Divider />
-            <CircularProgress size='xxl' />
-            <br />
-            <br />
-            <Text>Size: xxl</Text>
-            <Divider />
-            <CircularProgress size={40} />
-            <br />
-            <br />
-            <Text>Size: 40 (number)</Text>
-          </Card>
-        </div>
-        <div className='col-6'>
-          <Card title='Colors'>
-            <CircularProgress color='primary' />
-            <br />
-            <br />
-            <Text>Color: Primary</Text>
-            <Divider />
-            <CircularProgress color='primaryLight' />
-            <br />
-            <br />
-            <Text>Color: PrimaryLight</Text>
-            <Divider />
-            <CircularProgress color='secondary' />
-            <br />
-            <br />
-            <Text>Color: Secondary</Text>
-            <Divider />
-            <CircularProgress color='dark' />
-            <br />
-            <br />
-            <Text>Color: Dark</Text>
-            <Divider />
-            <CircularProgress color='medium' />
-            <br />
-            <br />
-            <Text>Color: Medium</Text>
-            <Divider />
-            <CircularProgress color='light' />
-            <br />
-            <br />
-            <Text>Color: Light</Text>
-            <Divider />
-            <CircularProgress color='success' />
-            <br />
-            <br />
-            <Text>Color: Success</Text>
-            <Divider />
-            <CircularProgress color='error' />
-            <br />
-            <br />
-            <Text>Color: Error</Text>
-            <Divider />
-            <CircularProgress color='info' />
-            <br />
-            <br />
-            <Text>Color: Info</Text>
-            <Divider />
-            <CircularProgress color='warning' />
-            <br />
-            <br />
-            <Text>Color: Warning</Text>
-          </Card>
-        </div>
-      </div>
+      <Card title='Default'>
+        <CircularProgress />
+        <br />
+        <br />
+        <Text>Size: md / Color: primary</Text>
+      </Card>
+      <Card title='Sizes'>
+        <CircularProgress size={40} />
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <CircularProgress size='xxl' />
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <CircularProgress size='xl' />
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <CircularProgress size='lg' />
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <CircularProgress size='md' />
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <CircularProgress size='sm' />
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <CircularProgress size='xs' />
+        <br />
+        <br />
+        <Text>Size: number / xxl / xl / lg / md / sm / xs</Text>
+      </Card>
+      <Card title='Colors'>
+        <CircularProgress color='primary' />
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <CircularProgress color='primaryLight' />
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <CircularProgress color='secondary' />
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <CircularProgress color='dark' />
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <CircularProgress color='medium' />
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <CircularProgress color='light' />
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <CircularProgress color='success' />
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <CircularProgress color='error' />
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <CircularProgress color='info' />
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <CircularProgress color='warning' />
+        <br />
+        <br />
+        <Text>
+          Colors: primary / primaryLight / secondary / dark / medium / light / success / error / info / warning
+        </Text>
+      </Card>
     </ContentLayout>
   )
 }
