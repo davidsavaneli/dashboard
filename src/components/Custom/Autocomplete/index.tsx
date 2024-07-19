39d9787dc20a40c4dc844393a4d581ac7efd0cbc
sava <savaneli.david@gmail.com>
2024-07-19 13:16:24 +0400

    fix autocomplete

--- src/components/Custom/Autocomplete/index.tsx
+++ src/components/Custom/Autocomplete/index.tsx
@@ -19,19 +19,30 @@ export interface AutocompleteProps<
 > extends Omit<MuiAutocompleteProps<T, Multiple, DisableClearable, FreeSolo>, 'renderInput'> {
   renderInput?: MuiAutocompleteProps<T, Multiple, DisableClearable, FreeSolo>['renderInput']
   label?: ReactNode
+  error?: boolean
+  helperText?: string
 }
 
 const Autocomplete = forwardRef<
   HTMLDivElement,
   AutocompleteProps<AutocompleteOption | string, boolean | undefined, boolean | undefined, boolean | undefined>
->(({ label, ...props }, ref) => {
+>(({ label, error, helperText, ...props }, ref) => {
   return (
     <FormControl>
       <MuiAutocomplete
-        disableCloseOnSelect
+        className={!props.multiple ? 'MuiAutocomplete-root-single' : ''}
+        disableCloseOnSelect={props.multiple}
         popupIcon={<Icon name='ArrowDown2' />}
         clearIcon={<Icon name='CloseCircle' />}
-        renderInput={(params) => <TextField className='MuiTextField-withIcon' {...params} label={label} />}
+        renderInput={(params) => (
+          <TextField
+            className='MuiTextField-withIcon'
+            {...params}
+            label={label}
+            error={error}
+            helperText={helperText}
+          />
+        )}
         renderTags={(value, getTagProps) =>
           value.map((option, index) => {
             const label = typeof option === 'string' ? option : option.label
--- src/pages/Autocomplete/index.tsx
+++ src/pages/Autocomplete/index.tsx
@@ -9,15 +9,64 @@ const autocompleteOptions = [
 const AutocompletePage = () => {
   return (
     <ContentLayout label='Autocomplete Component'>
-      <Card title='Autocomplete'>
-        <Autocomplete
-          multiple
-          options={autocompleteOptions}
-          label='Autocomplete'
-          onChange={(e, value) => console.log(value)}
-          loading
-        />
-      </Card>
+      <div className='row'>
+        <div className='col-6'>
+          <Card title='Default'>
+            <Autocomplete label='Label' options={autocompleteOptions} onChange={(e, value) => console.log(value)} />
+          </Card>
+        </div>
+        <div className='col-6'>
+          <Card title='Multiple'>
+            <Autocomplete
+              label='Label'
+              options={autocompleteOptions}
+              multiple
+              onChange={(e, value) => console.log(value)}
+            />
+          </Card>
+        </div>
+      </div>
+      <div className='row'>
+        <div className='col-6'>
+          <Card title='Helper Text'>
+            <Autocomplete
+              label='Label'
+              options={autocompleteOptions}
+              helperText='Helper Text'
+              onChange={(e, value) => console.log(value)}
+            />
+          </Card>
+        </div>
+        <div className='col-6'>
+          <Card title='Disabled'>
+            <Autocomplete
+              label='Label'
+              options={autocompleteOptions}
+              disabled
+              onChange={(e, value) => console.log(value)}
+            />
+          </Card>
+        </div>
+      </div>
+      <div className='row'>
+        <div className='col-6'>
+          <Card title='Errors'>
+            <Autocomplete
+              label='Label'
+              options={autocompleteOptions}
+              error
+              onChange={(e, value) => console.log(value)}
+            />
+            <Autocomplete
+              label='Label'
+              options={autocompleteOptions}
+              error
+              helperText='Helper Text'
+              onChange={(e, value) => console.log(value)}
+            />
+          </Card>
+        </div>
+      </div>
     </ContentLayout>
   )
 }
